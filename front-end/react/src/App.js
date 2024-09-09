3c8abd4aa3f90b2743661f1821494f405475275a
ArmandoJunior00 <armando.jr@sofintech.com.br>
2024-09-09 19:14:03 -0300

    formulario de cadastro primeiro commit

---
+++ front-end/react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import AppBar from './form/appbar';
+import Formulario from './form/formulario';
+
+function App() {
+  return (
+    <div className='App'>
+      <AppBar/>
+     <Formulario/>
+
+    </div>
+  );
+}
+
+export default App;
