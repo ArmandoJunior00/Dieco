7727ed08bfde4df563df8c8b0ac285f8eab94d2b
ArmandoJunior00 <armando.jr@sofintech.com.br>
2024-09-23 16:54:23 -0300

    back-end api se comunicando com o front

---
+++ back-end/Dieco/src/main/java/br/com/dieco/DiecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dieco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiecoApplication.class, args);
+	}
+
+}
---
+++ back-end/Dieco/src/test/java/br/com/dieco/DiecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dieco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
